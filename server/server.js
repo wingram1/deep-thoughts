c75ed2c98a42ddd718aced6481bf61abef9bc6ac
wingram1 <waingram96@gmail.com>
2022-05-04 10:26:20 -0500

    user, thought, users, thoughts queries

--- server/server.js
+++ server/server.js
@@ -1,14 +1,38 @@
-const express = require('express');
-const db = require('./config/connection');
+const express = require("express");
+// import ApolloServer
+const { ApolloServer } = require("apollo-server-express");
+
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
+// create a new Apollo server and pass in schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
+// function to create a new instance of Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  // integrate Apollo server with Express application as middleware
+  server.applyMiddleware({ app });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      // log where we an go to test our GQL API
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
   });
-});
+};
+
+startApolloServer(typeDefs, resolvers);
