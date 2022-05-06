037c345742c11430c6e79c58f0b63a620d87eb0f
wingram1 <waingram96@gmail.com>
2022-05-05 21:19:44 -0500

    reaction submission

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
 
-import ReactionList from '../components/ReactionList';
+import ReactionList from "../components/ReactionList";
+import ReactionForm from "../components/ReactionForm";
+import Auth from "../utils/auth";
 
-import { useQuery } from '@apollo/client';
-import { QUERY_THOUGHT } from '../utils/queries';
+import { useQuery } from "@apollo/client";
+import { QUERY_THOUGHT } from "../utils/queries";
 
 const SingleThought = (props) => {
   const { id: thoughtId } = useParams();
@@ -25,13 +27,15 @@ const SingleThought = (props) => {
         <p className="card-header">
           <span style={{ fontWeight: 700 }} className="text-light">
             {thought.username}
-          </span>{' '}
+          </span>{" "}
           thought on {thought.createdAt}
         </p>
         <div className="card-body">
           <p>{thought.thoughtText}</p>
         </div>
       </div>
+      {/* conditionally render reaction form */}
+      {Auth.loggedIn() && <ReactionForm thoughtId={thought._id} />}
 
       {thought.reactionCount > 0 && (
         <ReactionList reactions={thought.reactions} />
